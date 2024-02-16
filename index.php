5308cc2bd9ceaac4e77992921172b48092b1cde0
udintseva-ts <115084384+udintseva-ts@users.noreply.github.com>
2024-02-16 16:53:57 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
 <?php
+
+abstract class Product
+{
+    public static $income;
+    protected $price;
+
+    abstract function buy($weghtOrNumber);
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+}
+
+class UnitProduct extends Product
+{
+    public function buy($unitsNumber)
+    {
+        self::$income += $this->price * $unitsNumber;
+    }
+}
+
+
+class WeightProduct extends Product
+{
+    public function buy($weight)
+    {
+        self::$income += $this->price * $weight;
+    }
+}
+
+class DigitalProduct extends Product
+{
+    public $product;
+
+    public function buy($weightOrNumber)
+    {
+        self::$income += $this->product->buy($weightOrNumber / 2);
+    }
+
+    public function __construct($product)
+    {
+        $this->product = $product;
+    }
+}
