393704f55d1a791394d46d347bc107e08219c944
udintseva-ts <115084384+udintseva-ts@users.noreply.github.com>
2024-02-16 16:56:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,122 @@
 <?php
 
-abstract class Product
+class Product
 {
-    public static $income;
-    protected $price;
+    public $id;
+    public $title;
+    public $description;
+    public $price;
+    public $img;
+}
 
-    abstract function buy($weghtOrNumber);
-
-    public function __construct($price)
-    {
-        $this->price = $price;
+class ProductInCart extends Product
+{
+    public $amountInCart;
+    public function __construct(ProductInStock $product, int $amount) {
+        $this->id = $product->id;
+        $this->title = $product->title;
+        $this->price = $product->price;
+        $this->description = $product->description;
+        $this->img = $product->img;
+        $this->amountInCart = $amount;
     }
 }
 
-class UnitProduct extends Product
+class ProductInStock extends Product
 {
-    public function buy($unitsNumber)
-    {
-        self::$income += $this->price * $unitsNumber;
+    public $balanceInStock;
+    public function can_add_in_cart(int $amount) : bool {
+        if ($amount <= $this->balanceInStock) {
+            $this->balanceInStock -= $amount;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
-
-class WeightProduct extends Product
+class User
 {
-    public function buy($weight)
+    public $id;
+    public $firstName;
+    public $lastName;
+    public $birthDate;
+
+    public function __construct(int $id, string $firstName, string $lastName, DateTime $birthDate)
     {
-        self::$income += $this->price * $weight;
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->birthDate = $birthDate;
+    }
+}
+class Cart
+{
+    public $user;
+    private $products = [];
+
+    public function get_total_price() : int
+    {
+        $result = 0;
+        foreach ($this->products as $item) {
+            $result += $item->price * $item->amountInCart;
+        }
+        return $result;
+    }
+
+    public function add_product_in_cart(ProductInCart $product) : void
+    {
+        array_push($this->$product);
+    }
+
+    public function get_products_in_cart()
+    {
+        return $this->products;
+    }
+
+    public function __construct($user)
+    {
+        $this->user = $user;
     }
 }
 
-class DigitalProduct extends Product
+class Shop
 {
+    static function add_in_cart(ProductInStock $product, $amount, Cart $cart)
+    {
+        if ($product->can_add_in_cart($amount)) {
+            $productInCart = new ProductInCart($product, $amount);
+            $cart->add_product_in_cart($productInCart);
+        } else {
+            echo 'Недостаточно товаров на складе';
+        }
+    }
+}
+class Review
+{
+    public $user;
     public $product;
+    public $text;
+    private $rating;
 
-    public function buy($weightOrNumber)
+    public function __construct(User $user, Product $product, int $rating)
     {
-        self::$income += $this->product->buy($weightOrNumber / 2);
+        $this->user = $user;
+        $this->product = $product;
+        $this->set_rating($rating);
     }
 
-    public function __construct($product)
+    public function set_rating(int $value)
     {
-        $this->product = $product;
+        if ($value <= 5 and $value >= 1) {
+            $this->rating = $value;
+        } else {
+            echo 'Рейтинг должен быть от 1 до 5';
+        }
+    }
+
+    public function get_rating() : int
+    {
+        return $this->rating;
     }
 }
