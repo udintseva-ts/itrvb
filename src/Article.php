173e8849d8387621befd5f91925c9e500b4cf287
udintseva-ts <115084384+udintseva-ts@users.noreply.github.com>
2024-02-16 17:01:32 +0500

    Add files via upload

---
+++ src/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace my;
+
+class Article {
+    public $id;
+    public $author_id;
+    public $title;
+    public $text;
+}
+?>
---
+++ src/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace my;
+
+class User {
+    public $id;
+    public $name;
+    public $second_name;
+}
+
+?>
